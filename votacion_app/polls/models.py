edb66391df914289d1dc785c797e4d33bce745c1
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-02-18 17:16:21 -0300

    Creates Models: Question and Choice

--- votacion_app/polls/models.py
+++ votacion_app/polls/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
+from django.forms import DateTimeField
 
-# Create your models here.
+class Question(models.Model):
+    # id No es necesario, lo crea django solo
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # on_delete=models.CASCADE -> Si elemino una pregunta, elimina las resp asociadas a esa pregunta.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
