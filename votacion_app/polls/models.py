9da79e91524a2c6d83f617b483040e6c2c219300
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-02-18 18:40:32 -0300

    Adds two methods to Question

--- votacion_app/polls/models.py
+++ votacion_app/polls/models.py
@@ -1,15 +1,32 @@
 from pyexpat import model
 from statistics import mode
+from time import timezone
 from django.db import models
+import datetime
+
 from django.forms import DateTimeField
+from django.utils import timezone
 
 class Question(models.Model):
+
     # id No es necesario, lo crea django solo
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # datetime.timedelta(days=1) es igual a 1 día.
+
+
 class Choice(models.Model):
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # on_delete=models.CASCADE -> Si elemino una pregunta, elimina las resp asociadas a esa pregunta.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
