46523ceef5de6a6da67cd5be8e5132556d91aa4d
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-02-21 04:11:24 -0300

    Adds views and templates for: detail and vote

--- votacion_app/polls/views.py
+++ votacion_app/polls/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from select import select
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
-
-# Importo modulo que permite ejecutar respuesta http
-
-from django.http import HttpResponse
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse('Hello World')
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list':latest_question_list
+    })
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta {question_id}')
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # selected_choice = 'choice' in request.POST
+    # print(selected_choice)
+    try:
+        selected_choice = question.choice_set.get(pk=request.GET['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'No elegiste una respuesta.'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
